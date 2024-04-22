d7a0ceed6626cd3fe0dc73ae99e460544eca8436
AGYTgit <bubonb12@gmail.com>
2024-04-22 11:37:43 +0200

    Version 0.2
    added moving and rotating,
    restraints so the piece can't go out of bounds(works only for 'I')
    needs a way to count the distance from the bounds,
    more complex rotation system(so the piece knows where to move if rotation is not possible

--- block2.py
+++ block2.py
@@ -1,50 +1,57 @@
-"""color definitions for each piece (mainly for use in board_matrix)"""
-color_codes = {
-    'I': (0,255,255),
-    'J': (0,0,255),
-    'L': (255,127,0),
-    'O': (255,255,0),
-    'S': (0,255,0),
-    'T': (255,0,255),
-    'Z': (255,0,0),
-    }
-
-
-"""shape definitions for each piece (mainly for use in board_matrix)"""
-shape_codes = {
-    'I': [[1,1,1,1]],
-
-    'J': [[0,1],
-          [0,1],
-          [1,1]],
-
-    'L': [[1,0],
-          [1,0],
-          [1,1]],
-
-    'O': [[1,1],
-          [1,1]],
-
-    'S': [[0,1,1],
-          [1,1,0]],
-
-    'T': [[0,1,0],
-          [1,1,1]],
-
-    'Z': [[1,1,0],
-          [0,1,1]],
-    }
-
+import block_codes
 
 class Block:
-    def __init__(self, block_code, pos_x, pos_y):
-        """get block code"""
-        self.block_code = block_code
+      def __init__(self, block_code, pos_x, pos_y, rotation=0):
+            """get block code"""
+            self.block_code = block_code
 
-        """get color and shape"""
-        self.color = color_codes[block_code]
-        self.shape = shape_codes[block_code]
+            """get x/y position"""
+            self.pos_x = pos_x
+            self.pos_y = pos_y
 
-        """get x/y position"""
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+            """get rotation"""
+            self.rotation = rotation
+
+            """get color and shape"""
+            self.color = block_codes.color_codes[block_code]
+            self.shape = block_codes.shape_codes[block_code][rotation]
+
+      def rotate(self, board_matrix):
+            self.remove_from_board_matrix(board_matrix)
+
+            """rotate the block clockwise"""
+            self.rotation = (self.rotation + 1) % 4
+            self.shape = block_codes.shape_codes[self.block_code][self.rotation]
+
+            self.add_to_board_matrix(board_matrix)
+            
+
+      def move(self, board_matrix, direction='DOWN'):
+
+            self.remove_from_board_matrix(board_matrix)
+
+            """move block"""
+            if  direction == "LEFT" and self.pos_x > 0:
+                  self.pos_x -= 1
+            elif  direction == "RIGHT" and self.pos_x < len(board_matrix[0]) - 4:
+                  self.pos_x += 1
+            elif direction == "DOWN" and self.pos_y < len(board_matrix) - 1:
+                  self.pos_y += 1
+
+            self.add_to_board_matrix(board_matrix)
+           
+      
+      def remove_from_board_matrix(self, board_matrix):
+            """remove block from board_matrix"""
+            for i in range(len(self.shape)):
+                  for j in range(len(self.shape[i])):
+                        if self.shape[i][j] == 1:
+                              board_matrix[i + self.pos_y][j + self.pos_x][1] = 'B'
+
+
+      def add_to_board_matrix(self, board_matrix):
+            """add block to board_matrix"""
+            for i in range(len(self.shape)):
+                  for j in range(len(self.shape[i])):
+                        if self.shape[i][j] == 1:
+                              board_matrix[i + self.pos_y][j + self.pos_x][1] = self.block_code
--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 import button
 import block2
+import block_codes
 
 
 """initialize pygame and create the screen object."""
@@ -21,24 +22,42 @@ grid_size = 30
 border_thickness = 50
 
 
+# """2D list of cells on the board"""
+# board_matrix = [[['X','B'] for _ in range(board_width + 2)] for _ in range(board_height + 2)]
+
+
+# """draw game board and update board_matrix"""
+# def draw_board():
+#     for i in range(board_height + 2):
+#         for j in range(board_width + 2):
+#             if i > 0 and i < board_height + 1 and \
+#                 j > 0 and j < board_width + 1:
+#                 if board_matrix[i][j][0] != board_matrix[i][j][1]:
+#                     if board_matrix[i][j][1] == 'B':
+#                         pygame.draw.rect(window, (50,50,50), ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
+#                         pygame.draw.rect(window, (30,30,30), ((window_width - board_width * grid_size) / 2 + 1 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + 1 + (i - 1) * grid_size, grid_size - 2, grid_size - 2))
+#                     else:
+#                         pygame.draw.rect(window, block_codes.color_codes[board_matrix[i][j][1]], ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
+                        
+#                     board_matrix[i][j][0] = board_matrix[i][j][1]
+
+
 """2D list of cells on the board"""
-board_matrix = [[['X','B'] for _ in range(board_width + 2)] for _ in range(board_height + 2)]
+board_matrix = [[['X','B'] for _ in range(board_width)] for _ in range(board_height)]
 
 
 """draw game board and update board_matrix"""
 def draw_board():
-    for i in range(board_height + 2):
-        for j in range(board_width + 2):
-            if i > 0 and i < board_height + 1 and \
-                j > 0 and j < board_width + 1:
-                if board_matrix[i][j][0] != board_matrix[i][j][1]:
-                    if board_matrix[i][j][1] == 'B':
-                        pygame.draw.rect(window, (50,50,50), ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
-                        pygame.draw.rect(window, (30,30,30), ((window_width - board_width * grid_size) / 2 + 1 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + 1 + (i - 1) * grid_size, grid_size - 2, grid_size - 2))
-                    else:
-                        pygame.draw.rect(window, block2.color_codes[board_matrix[i][j][1]], ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
-                        
-                    board_matrix[i][j][0] = board_matrix[i][j][1]
+    for i in range(board_height):
+        for j in range(board_width):
+            if board_matrix[i][j][0] != board_matrix[i][j][1]:
+                if board_matrix[i][j][1] == 'B':
+                    pygame.draw.rect(window, (50,50,50), ((window_width - board_width * grid_size) / 2 + j * grid_size, (window_height - board_height * grid_size) / 2 + i * grid_size, grid_size, grid_size))
+                    pygame.draw.rect(window, (30,30,30), ((window_width - board_width * grid_size) / 2 + 1 + j * grid_size, (window_height - board_height * grid_size) / 2 + 1 + i * grid_size, grid_size - 2, grid_size - 2))
+                else:
+                    pygame.draw.rect(window, block_codes.color_codes[board_matrix[i][j][1]], ((window_width - board_width * grid_size) / 2 + j * grid_size, (window_height - board_height * grid_size) / 2 + i * grid_size, grid_size, grid_size))
+                    
+                board_matrix[i][j][0] = board_matrix[i][j][1]
 
 
 """button init"""
@@ -46,13 +65,24 @@ red_button = button.Button(pygame, window, 10,10,50,50, (150,0,0), "red")
 green_button = button.Button(pygame, window, 10,70,50,50, (0,150,0), "green")
 blue_button = button.Button(pygame, window, 10,130,50,50, (0,0,150), "blue")
 
+left_button = button.Button(pygame, window, 10,190,50,50, (100,100,100), "<")
+right_button = button.Button(pygame, window, 70,190,50,50, (100,100,100), ">")
+
 
 """draw everything"""
 def draw():
+    """border around game board"""
     pygame.draw.rect(window, (50,50,50), ((window_width - board_width * grid_size - border_thickness) / 2, (window_height - board_height * grid_size - border_thickness) / 2, board_width * grid_size + border_thickness, board_height * grid_size + border_thickness))
+    
+    """buttons"""
     red_button.draw()
     green_button.draw()
     blue_button.draw()
+
+    left_button.draw()
+    right_button.draw()
+    
+    """game board"""
     draw_board()
 
 draw()
@@ -76,44 +106,52 @@ while running:
             """get called on each 100th tick (1 second)"""
             if tick % 100 == 0:
                 try:
-                    """remove block from board_matrix"""
-                    for i in range(len(first_block.shape)):
-                        for j in range(len(first_block.shape[0])):
-                            if first_block.shape[i][j] == 1:
-                                board_matrix[i + first_block.pos_y][j + first_block.pos_x][1] = 'B'
-
-                    """move block"""
-                    first_block.pos_y += 1
-
-                    """add block to board_matrix"""
-                    for i in range(len(first_block.shape)):
-                        for j in range(len(first_block.shape[0])):
-                            if first_block.shape[i][j] == 1:
-                                board_matrix[i + first_block.pos_y][j + first_block.pos_x][1] = first_block.block_code
+                    first_block.move(board_matrix, 'DOWN')
                 except Exception as e:
                     print(e)
 
 
+    # rotate block
     if red_button.pressed():
         print("Red")
-        # start game
+        try:
+            first_block.rotate(board_matrix)
+        except Exception as e:
+            print(e)
 
+    # init block
     elif green_button.pressed():
         print("green")
 
         """init block"""
-        first_block = block2.Block('I', 4, 1)
+        first_block = block2.Block('I', 3, 0)
 
         """add block to board_matrix"""
-        for i in range(len(first_block.shape)):
-            for j in range(len(first_block.shape[0])):
-                if first_block.shape[i][j] == 1:
-                    board_matrix[i + first_block.pos_y][j + first_block.pos_x][1] = first_block.block_code
+        first_block.add_to_board_matrix(board_matrix)
 
+    # print board_matrix
     elif blue_button.pressed():
         print("blue")
-        for i in range(board_height + 2):
+        for i in range(board_height):
             print(board_matrix[i], i)
+
+    # move block left
+    elif left_button.pressed():
+        print("left")
+        try:
+            first_block.move(board_matrix, 'LEFT')
+        except Exception as e:
+            print(e)
+        print(first_block.pos_x)
+
+    # move block right
+    elif right_button.pressed():
+        print("right")
+        try:
+            first_block.move(board_matrix, 'RIGHT')
+        except Exception as e:
+            print(e)
+        print(first_block.pos_x)
 
 
     draw_board()
