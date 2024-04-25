38ebdb8a06be54d1c3928be3833216a215b3c396
AGYTgit <bubonb12@gmail.com>
2024-04-25 12:24:45 +0200

    Version 0.3
    added wall-kick for all pieces,
    fixed a bug where blocks would go through each other if they were on the edge of the board

--- block.py
+++ block.py
@@ -16,99 +16,159 @@ class Block:
             self.color = block_codes.color[block_code]
             self.shape = block_codes.shape[block_code][rotation]
             self.offset = block_codes.offset[block_code][rotation]
+            if self.block_code == 'I':
+                  self.wall_kick_offset = block_codes.wall_kick_offset['I']
+            else:
+                  self.wall_kick_offset = block_codes.wall_kick_offset['other']
 
 
-      def rotate(self, board_matrix, direction="CW"):
-            board_matrix = self.remove_from_board_matrix(board_matrix)
+      def safe_rotate(self, board_matrix, direction="CW"):
+            """remove block from board_matrix and store updated board_matrix"""
+            board_matrix = self.remove_block_from_board_matrix(board_matrix)
 
             """set new rotation"""
-            if direction == "CW":
-                  self.rotation = (self.rotation - 1 + 4) % 4
-            elif direction == "CCW":
-                  self.rotation = (self.rotation + 1) % 4
+            self.rotate(direction)
 
-            """update shape's rotation"""
-            self.shape = block_codes.shape[self.block_code][self.rotation]
-            self.offset = block_codes.offset[self.block_code][self.rotation]
+            """update block's shape, offset and wall-kick offset"""
+            self.update_codes(shape=True, offset=True, wall_kick_offset=True)
 
-            """set new rotation if position is valid"""
-            if self.validate_position(board_matrix) == True:
-                  self.add_to_board_matrix(board_matrix)
+            """go back to old rotation"""
+            self.rotate(direction, reverse=True)
+
+            """loop through wall-kick options"""
+            for wall_kick_offset in self.wall_kick_offset[(self.rotation,direction)]:
+                  """check if position is valid"""
+                  validation, offset = self.validate_position(board_matrix, wall_kick_offset)
+                  if validation == True:
+                        print(offset)
+                        """set new rotation"""
+                        self.rotate(direction)
+
+                        """apply offset"""
+                        self.pos_y += offset[1]
+                        self.pos_x += offset[0]
+
+                        """add block to board_matrix"""
+                        self.add_block_to_board_matrix(board_matrix)
+                        break
             else:
-                  """go back to old rotation"""
-                  if direction == "CW":
-                        self.rotation = (self.rotation + 1) % 4
-                  elif direction == "CCW":
-                        self.rotation = (self.rotation - 1 + 4) % 4
-
                   """update shape's rotation"""
                   self.shape = block_codes.shape[self.block_code][self.rotation]
                   self.offset = block_codes.offset[self.block_code][self.rotation]
 
-                  self.add_to_board_matrix(board_matrix)
+                  """add block to board_matrix"""
+                  self.add_block_to_board_matrix(board_matrix)
             
 
-      def move(self, board_matrix, direction='DOWN'):
-            self.remove_from_board_matrix(board_matrix)
+      def safe_move(self, board_matrix, direction='DOWN'):
+            """remove block from board_matrix and store updated board_matrix"""
+            self.remove_block_from_board_matrix(board_matrix)
 
             """move block"""
-            if  direction == "LEFT" and self.pos_x > 0 - self.offset[0]:
-                  self.pos_x -= 1
-            elif  direction == "RIGHT" and self.pos_x < len(board_matrix[0]) - len(self.shape[0]) - self.offset[0]:
-                  self.pos_x += 1
-            elif direction == "DOWN" and self.pos_y < len(board_matrix) - 1 - self.offset[1]:
-                  self.pos_y += 1
-            elif direction == "UP" and self.pos_y > 0 - self.offset[1]:
-                  self.pos_y -= 1
+            self.move(board_matrix, direction)
 
-            if self.validate_position(board_matrix):
-                  self.add_to_board_matrix(board_matrix)
+            validation, _ = self.validate_position(board_matrix)
+            if validation:
+                  """add block to board_matrix"""
+                  self.add_block_to_board_matrix(board_matrix)
             else:
                   """move back to old position"""
-                  if  direction == "LEFT" and self.pos_x > 0 - self.offset[0]:
-                        self.pos_x += 1
-                  elif  direction == "RIGHT" and self.pos_x < len(board_matrix[0]) - len(self.shape[0]) - self.offset[0]:
-                        self.pos_x -= 1
-                  elif direction == "DOWN" and self.pos_y < len(board_matrix) - 1 - self.offset[1]:
-                        self.pos_y -= 1
-                  elif direction == "UP" and self.pos_y > 0 - self.offset[1]:
-                        self.pos_y += 1
+                  self.move(board_matrix, direction, reverse=True)
            
-                  self.add_to_board_matrix(board_matrix)
+                  """add block to board_matrix"""
+                  self.add_block_to_board_matrix(board_matrix)
       
       
-      def remove_from_board_matrix(self, board_matrix):
+      def remove_block_from_board_matrix(self, board_matrix):
             """remove block from board_matrix"""
             for i in range(len(self.shape)):
                   for j in range(len(self.shape[i])):
-                        if self.shape[i][j] == 1:
-                              board_matrix[i + self.pos_y + self.offset[1]][j + self.pos_x + self.offset[0]][1] = 'B'
+                        """filter out empty parts of the block"""
+                        if self.shape[i][j] == 0:
+                              continue
+                        """remove block part"""
+                        board_matrix[i + self.pos_y + self.offset[1]][j + self.pos_x + self.offset[0]][1] = 'B'
             return board_matrix
 
 
-      def add_to_board_matrix(self, board_matrix):
+      def add_block_to_board_matrix(self, board_matrix):
             """add block to board_matrix"""
             for i in range(len(self.shape)):
                   for j in range(len(self.shape[i])):
-                        if self.shape[i][j] == 1:
-                              board_matrix[i + self.pos_y + self.offset[1]][j + self.pos_x + self.offset[0]][1] = self.block_code
+                        """filter out empty parts of the block"""
+                        if self.shape[i][j] == 0:
+                              continue
+                        """add block part"""
+                        board_matrix[i + self.pos_y + self.offset[1]][j + self.pos_x + self.offset[0]][1] = self.block_code
             return board_matrix
 
 
-      def validate_position(self, board_matrix):
+      def validate_position(self, board_matrix, wall_kick_offset=[0,0]):
             """check if new position is valid"""
             for i in range(len(self.shape)):
                   for j in range(len(self.shape[i])):
-                        """filter out where the piece is"""
+                        """filter out empty parts of the block"""
                         if self.shape[i][j] == 0:
                               continue
                         """chekc if piece is inside the board"""
-                        if  0 <= self.pos_y + self.offset[1] + i <= 19 and \
-                              0 <= self.pos_x + self.offset[0] + j <= 9:
+                        if  0 <= self.pos_y + self.offset[1] + i + wall_kick_offset[1] <= 19 and \
+                              0 <= self.pos_x + self.offset[0] + j + wall_kick_offset[0] <= 9:
                               """check for collision"""
-                              if board_matrix[self.pos_y + self.offset[1] + i][self.pos_x + self.offset[0] + j][1] == 'B':
+                              if board_matrix[self.pos_y + self.offset[1] + i + wall_kick_offset[1]][self.pos_x + self.offset[0] + j + wall_kick_offset[0]][1] == 'B':
                                     continue
-                        return False           
+                        return False, wall_kick_offset
             else:
-                  return True
+                  return True, wall_kick_offset
       
+
+      def move(self, board_matrix, direction, reverse=False):
+            """move block"""
+            if not reverse:
+                  if  direction == "LEFT" and self.pos_x > 0 - self.offset[0]:
+                        self.pos_x -= 1
+                  elif  direction == "RIGHT" and self.pos_x < len(board_matrix[0]) - len(self.shape[0]) - self.offset[0]:
+                        self.pos_x += 1
+                  elif direction == "UP" and self.pos_y > 0 - self.offset[1]:
+                        self.pos_y -= 1
+                  elif direction == "DOWN" and self.pos_y < len(board_matrix) - 1 - self.offset[1]:
+                        self.pos_y += 1
+            else:
+                  if  direction == "RIGHT" and self.pos_x > 0 - self.offset[0]:
+                        self.pos_x -= 1
+                  elif  direction == "LEFT" and self.pos_x < len(board_matrix[0]) - len(self.shape[0]) - self.offset[0]:
+                        self.pos_x += 1
+                  elif direction == "DOWN" and self.pos_y > 0 - self.offset[1]:
+                        self.pos_y -= 1
+                  elif direction == "UP" and self.pos_y < len(board_matrix) - 1 - self.offset[1]:
+                        self.pos_y += 1
+
+
+      def rotate(self, direction, reverse=False):
+            """set new rotation"""
+            if direction == "CW":
+                  if not reverse:
+                        self.rotation = (self.rotation - 1 + 4) % 4
+                  else:
+                        self.rotation = (self.rotation + 1) % 4
+            elif direction == "CCW":
+                  if not reverse:
+                        self.rotation = (self.rotation + 1) % 4
+                  else:
+                        self.rotation = (self.rotation - 1 + 4) % 4
+            else:
+                  raise ValueError("Invalid direction argument for safe_rotate()")
+      
+
+      def update_codes(self, color=False, shape=False, offset=False, wall_kick_offset=False):
+            """update block's color, shape, offset and wall-kick offset"""
+            if color:
+                  self.color = block_codes.color[self.block_code]
+            if shape:
+                  self.shape = block_codes.shape[self.block_code][self.rotation]
+            if offset:
+                  self.offset = block_codes.offset[self.block_code][self.rotation]
+            if wall_kick_offset:
+                  if self.block_code == 'I':
+                        self.wall_kick_offset = block_codes.wall_kick_offset['I']
+                  else:
+                        self.wall_kick_offset = block_codes.wall_kick_offset['other']
--- main.py
+++ main.py
@@ -22,26 +22,6 @@ grid_size = 30
 border_thickness = 50
 
 
-# """2D list of cells on the board"""
-# board_matrix = [[['X','B'] for _ in range(board_width + 2)] for _ in range(board_height + 2)]
-
-
-# """draw game board and update board_matrix"""
-# def draw_board():
-#     for i in range(board_height + 2):
-#         for j in range(board_width + 2):
-#             if i > 0 and i < board_height + 1 and \
-#                 j > 0 and j < board_width + 1:
-#                 if board_matrix[i][j][0] != board_matrix[i][j][1]:
-#                     if board_matrix[i][j][1] == 'B':
-#                         pygame.draw.rect(window, (50,50,50), ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
-#                         pygame.draw.rect(window, (30,30,30), ((window_width - board_width * grid_size) / 2 + 1 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + 1 + (i - 1) * grid_size, grid_size - 2, grid_size - 2))
-#                     else:
-#                         pygame.draw.rect(window, block_codes.color_codes[board_matrix[i][j][1]], ((window_width - board_width * grid_size) / 2 + (j - 1) * grid_size, (window_height - board_height * grid_size) / 2 + (i - 1) * grid_size, grid_size, grid_size))
-                        
-#                     board_matrix[i][j][0] = board_matrix[i][j][1]
-
-
 """2D list of cells on the board"""
 board_matrix = [[['X','B'] for _ in range(board_width)] for _ in range(board_height)]
 
@@ -65,11 +45,14 @@ red_button = button.Button(pygame, window, 10,10,50,50, (150,0,0), "red")
 green_button = button.Button(pygame, window, 10,70,50,50, (0,150,0), "green")
 blue_button = button.Button(pygame, window, 10,130,50,50, (0,0,150), "blue")
 
-left_button = button.Button(pygame, window, 10,250,50,50, (100,100,100), "<")
-right_button = button.Button(pygame, window, 70,250,50,50, (100,100,100), ">")
-
 rotate_clockwise_button = button.Button(pygame, window, 10,190,50,50, (100,100,100), "<")
 rotate_counterclockwise_button = button.Button(pygame, window, 70,190,50,50, (100,100,100), ">")
+
+left_button = button.Button(pygame, window, 10,280,50,50, (100,100,100), "<")
+right_button = button.Button(pygame, window, 130,280,50,50, (100,100,100), ">")
+up_button = button.Button(pygame, window, 70,250,50,50, (100,100,100), "^")
+down_button = button.Button(pygame, window, 70,310,50,50, (100,100,100), "⌄")
+
 
 
 """draw everything"""
@@ -81,12 +64,15 @@ def draw():
     red_button.draw()
     green_button.draw()
     blue_button.draw()
+    
+    rotate_clockwise_button.draw()
+    rotate_counterclockwise_button.draw()
 
     left_button.draw()
     right_button.draw()
+    up_button.draw()
+    down_button.draw()
 
-    rotate_clockwise_button.draw()
-    rotate_counterclockwise_button.draw()
     
     """game board"""
     draw_board()
@@ -111,24 +97,27 @@ while running:
 
             """get called on each 100th tick (1 second)"""
             if tick % 100 == 0:
-                try:
-                    first_block.move(board_matrix, 'DOWN')
-                except Exception as e:
-                    print(e)
+                pass
+                # try:
+                #     first_block.move(board_matrix, 'DOWN')
+                # except Exception as e:
+                #     print(e)
 
 
     if red_button.pressed():
-        print("Red")
+        """init block"""
+        first_block = block.Block('O', 4, 0)
+
+        """add block to board_matrix"""
+        first_block.add_block_to_board_matrix(board_matrix)
 
     # init block
     elif green_button.pressed():
-        print("green")
-
         """init block"""
-        first_block = block.Block('Z', 3, 0)
+        first_block = block.Block('I', 3, 0)
 
         """add block to board_matrix"""
-        first_block.add_to_board_matrix(board_matrix)
+        first_block.add_block_to_board_matrix(board_matrix)
 
     # print board_matrix
     elif blue_button.pressed():
@@ -136,37 +125,45 @@ while running:
         for i in range(board_height):
             print(board_matrix[i], i)
 
-    # move block left
-    elif left_button.pressed():
-        print("left")
-        try:
-            first_block.move(board_matrix, 'LEFT')
-            print(first_block.pos_x)
-        except Exception as e:
-            print(e)
-
-    # move block right
-    elif right_button.pressed():
-        print("right")
-        try:
-            first_block.move(board_matrix, 'RIGHT')
-            print(first_block.pos_x)
-        except Exception as e:
-            print(e)
-
     # rotate block CW
     elif rotate_clockwise_button.pressed():
-        print("clockwise")
         try:
-            first_block.rotate(board_matrix, "CW")
+            first_block.safe_rotate(board_matrix, "CW")
         except Exception as e:
             print(e)
 
     # rotate block CCW
     elif rotate_counterclockwise_button.pressed():
-        print("counterclockwise")
         try:
-            first_block.rotate(board_matrix, "CCW")
+            first_block.safe_rotate(board_matrix, "CCW")
+        except Exception as e:
+            print(e)
+            
+    # move block left
+    elif left_button.pressed():
+        try:
+            first_block.safe_move(board_matrix, 'LEFT')
+        except Exception as e:
+            print(e)
+
+    # move block right
+    elif right_button.pressed():
+        try:
+            first_block.safe_move(board_matrix, 'RIGHT')
+        except Exception as e:
+            print(e)
+
+    # move block up
+    elif up_button.pressed():
+        try:
+            first_block.safe_move(board_matrix, 'UP')
+        except Exception as e:
+            print(e)
+
+    # move block down
+    elif down_button.pressed():
+        try:
+            first_block.safe_move(board_matrix, 'DOWN')
         except Exception as e:
             print(e)
 
